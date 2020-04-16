076a998cba14265c5cd9ce17cb8a36a069093580
git-aditi <aditi.shrivastava@students.iiit.ac.in>
2020-04-16 14:40:46 +0530

    Removed old repository

--- flask_module/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-# Creates the application object 
-app = Flask(__name__)
-
-# Import Views from the app module. (DO NOT Confuse with app variable)
-from app import views
-
-# Import is at the end to avoid circular reference
